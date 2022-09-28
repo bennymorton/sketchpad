caf0d952afb1315d71b3ecd04ac2cebe388f6890
Ben <benjaminmorton@live.co.uk>
2022-09-28 13:02:09 +0100

    Responsive grid created, with user input. C ould not work out how to make grid grow smaller based on user input, after it has grown to a bigger valu e

--- sketchpad.js
+++ sketchpad.js
@@ -1,10 +1,30 @@
-container = document.querySelector('.container');
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        let box = document.createElement('div');
-        box.style.gridRow = i+1;
-        box.style.gridColumn = j+1;
-        container.appendChild(box);
-    }    
+function hover(element) {
+    element.classList.add('hover');
+    setTimeout(() => element.classList.remove('hover'), 1000)
 }
+
+const container = document.querySelector('.container');
+
+function makeGrid(squares){
+    if (typeof box !== 'undefined') {
+        container.innerHTML = "";
+    }
+    for (let i = 0; i < squares; i++) {
+        for (let j = 0; j < squares; j++) {
+            let box = document.createElement('div');
+            box.style.gridRow = i+1;
+            box.style.gridColumn = j+1;
+            container.appendChild(box);
+            box.addEventListener('mouseover', () => hover(box));
+        }    
+    }
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    // do {
+        let squares = prompt('How many squares tall and wide would you like the grid?')
+    // } while (squares > 100)
+
+    makeGrid(squares);
+});
